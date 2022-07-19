51d38ff450af9767d5d673baed5115ba6a407149
Michael Phillips <benmont@users.noreply.github.com>
2022-07-19 12:53:26 -0500

    Update config.py

--- config.py
+++ config.py
@@ -3,3 +3,4 @@ ClientSecret = "TxRBilcHdC6WGBee]fs?QR:SJ8nI[g82"
 Scopes = ['https://outlook.office.com/IMAP.AccessAsUser.All','https://outlook.office.com/SMTP.Send']
 RefreshTokenFileName = "imap_smtp_refresh_token"
 AccessTokenFileName = "imap_smtp_access_token"
+# Authority = "https://login.microsoftonline.com/XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX/"
