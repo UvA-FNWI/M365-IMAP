6c67088b8dc3de20d017ab22595a16998f9b64fa
Gerrit Oomens <g.oomens@uva.nl>
2022-09-13 08:17:25 +0200

    Default to no authority
    Resolves #13

--- config.py
+++ config.py
@@ -3,4 +3,6 @@ ClientSecret = "TxRBilcHdC6WGBee]fs?QR:SJ8nI[g82"
 Scopes = ['https://outlook.office.com/IMAP.AccessAsUser.All','https://outlook.office.com/SMTP.Send']
 RefreshTokenFileName = "imap_smtp_refresh_token"
 AccessTokenFileName = "imap_smtp_access_token"
-# Authority = "https://login.microsoftonline.com/XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX/"
+
+# Optionally specify a tenantId here: "https://login.microsoftonline.com/XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX/"
+Authority = None 
